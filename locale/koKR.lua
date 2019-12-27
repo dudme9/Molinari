b9ddef8d2c3e3174c4d075ab372c9affad0d3f2f
dudme9 <59275387+dudme9@users.noreply.github.com>
2019-12-27 15:22:43 +0900

    Update koKR.lua

--- locale/koKR.lua
+++ locale/koKR.lua
@@ -5,6 +5,6 @@ L['ALT + CTRL key'] = ALT_KEY_TEXT .. ' + ' .. CTRL_KEY
 L['ALT + SHIFT key'] = ALT_KEY_TEXT .. ' + ' .. SHIFT_KEY
 
 -- config
--- L['Drag items into the window below to add more.'] = '' -- MISSING TRANSLATION
--- L['Items blacklisted from potentially being processed.'] = '' -- MISSING TRANSLATION
--- L['Right-click to remove item'] = '' -- MISSING TRANSLATION
+L['Drag items into the window below to add more.'] = '더 추가하려면 아래의 창으로 아이템 드래그'
+L['Items blacklisted from potentially being processed.'] = '잠재적으로 처리되지 않아 차단된 아이템'
+L['Right-click to remove item'] = '우클릭하여 아이템 제거'
